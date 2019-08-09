0ba8b769da81109f4b89e5b744c61a14fea64d45
Lester Yang <lester842634640@gmail.com>
2019-08-09 17:25:52 -0400

    Day 12

--- 12_Regular-Expression/SplitSentence.py
+++ 12_Regular-Expression/SplitSentence.py
@@ -2,6 +2,7 @@ import re
 
 def main():
     f = open('Dian-Jiang-Chun.txt','r',encoding='utf-8')
+    f_s = open('Dian-Jiang-Chun-Split.txt','w',encoding='utf-8')
     poem = f.read()
     sentences = re.split(r'[,.，。\n ]',poem)
     # ['蹴罢秋千', '起来慵整纤纤手', '', '露浓花瘦', '薄汗轻衣透', '',
@@ -11,7 +12,9 @@ def main():
         sentences.remove('')
     for st in sentences:
         print(st)
+        f_s.write(st + '\n')
     f.close()
+    f_s.close()
 
 if __name__ == "__main__":
     main()
